1d465a3f7c70d78bc39a6718c7501aa4cd638900
Paul Cohen <paul.cohen@seibostudios.se>
2018-12-02 14:36:27 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My solutions in Python to Advent of Code 2018
+
+This repository contains my solutions to [Advent of code 2018](https://adventofcode.com/).
---
+++ day1/day1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""This program is a solution to https://adventofcode.com/2018/day/1"""
+
+import sys
+import argparse
+
+DATA_FILE = "input.dat"
+
+def solve_problem (verbose=False):
+    """Solve the problem."""
+    result = 0
+    f = open (DATA_FILE)
+    i = 1
+    for line in f:
+        try:
+            result += int (line)
+        except ValueError:
+            print ("Value error on line %d: '%s'" % (i, line.strip ()))
+            sys.exit (1)
+        i += 1
+    print (result)
+
+def main():
+    parser = argparse.ArgumentParser (description='This program solves the Advent\
+                                      of code problem https://adventofcode.com/2018/day/1')
+    parser.add_argument ('-v', '--verbose', action='store_true', default=False,
+                         help="print info about what's going on [False]")
+    args = parser.parse_args ()
+    solve_problem (args.verbose)
+    sys.exit (0)
+
+if __name__ == "__main__":
+    main()
